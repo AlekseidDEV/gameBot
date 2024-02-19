68d91125dc5eb849c87d913c05158c62c68f8c67
Alex <lesha.rudiko@gmail.com>
2024-02-19 09:36:58 +0300

    new lesson

---
+++ main.js
@@ -0,0 +1,51 @@
+"use strict"
+
+const gameBot = function(maxValue, attemptsNum){
+
+    let randomNumber = Math.floor(Math.random() * maxValue)
+    let attempts = attemptsNum
+
+    const examConfirm = function(boolValue){
+        if(boolValue){
+            attempts = attemptsNum
+            programmBot()
+        } else{
+            return
+        }
+    }
+
+    const programmBot = function(){
+        let answer = prompt('Угадай число от 1 до 100')
+        let restartGame
+        
+        if(answer === null){
+            alert('Игра окончена')
+            return
+        } else if(isNaN(answer)){
+            alert('Введите число')
+            programmBot()
+        }
+
+        switch(true){
+            case +answer > randomNumber && attempts > 0:
+                attempts--
+                alert(`загаданное число меньше, попыток осталось ${attempts}`)
+                programmBot()
+                break
+            case +answer < randomNumber && attempts > 0:
+                attempts--
+                alert(`загаданное число больше, попыток осталось ${attempts}`)
+                programmBot()
+                break
+            case +answer === randomNumber:
+                alert(`Поздравляю, вы угадали, загаданное число ${randomNumber}`)
+                break
+            case attempts === 0:
+                restartGame = confirm(`Попытки кончились, хотите сыграть еще ?`)
+                examConfirm(restartGame)
+        }
+    }
+    programmBot()
+}
+
+gameBot(101, 10)
